2f0813f4a97fc218143e5025f7e43cb88e5ebc41
Gerard <gerardfabrego@gmail.com>
2024-01-22 15:32:14 +0100

    Move world logic into its own plugin

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,15 @@
 use bevy::prelude::*;
-use camera::CameraPlugin;
-use player::PlayerPlugin;
 
 mod camera;
 mod player;
+mod world;
+
+use camera::CameraPlugin;
+use player::PlayerPlugin;
+use world::WorldPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, PlayerPlugin, CameraPlugin))
-        .add_systems(Startup, (spawn_light, spawn_floor))
+        .add_plugins((DefaultPlugins, PlayerPlugin, CameraPlugin, WorldPlugin))
         .run();
-}
-
-fn spawn_light(mut commands: Commands) {
-    let light = PointLightBundle {
-        point_light: PointLight {
-            intensity: 2000.0,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 5.0, 0.0),
-        ..default()
-    };
-
-    commands.spawn(light);
-}
-
-fn spawn_floor(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let floor = PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
-        material: materials.add(Color::DARK_GREEN.into()),
-        ..default()
-    };
-
-    commands.spawn(floor);
 }

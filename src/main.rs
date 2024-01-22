ce34a7602fbf0e0c55e6585759df81fc11ea079f
Gerard <gerardfabrego@gmail.com>
2024-01-22 15:21:38 +0100

    Spawn player

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,39 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_camera, spawn_floor))
+        .add_systems(
+            Startup,
+            (spawn_camera, spawn_light, spawn_floor, spawn_player),
+        )
         .run();
+}
+
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let player: MaterialMeshBundle<StandardMaterial> = PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+        material: materials.add(Color::BLUE.into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    };
+
+    commands.spawn(player);
+}
+
+fn spawn_light(mut commands: Commands) {
+    let light = PointLightBundle {
+        point_light: PointLight {
+            intensity: 2000.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 5.0, 0.0),
+        ..default()
+    };
+
+    commands.spawn(light);
 }
 
 fn spawn_camera(mut commands: Commands) {

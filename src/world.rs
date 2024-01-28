5c7e1b9be59e67399f91797183de52f32dd2623e
Gerard <gerardfabrego@gmail.com>
2024-01-28 10:25:34 +0100

    Add more obstacles

--- src/world.rs
+++ src/world.rs
@@ -14,6 +14,7 @@ fn spawn_light(mut commands: Commands) {
         PointLightBundle {
             point_light: PointLight {
                 intensity: 2000.0,
+                shadows_enabled: true,
                 ..default()
             },
             transform: Transform::from_xyz(0.0, 5.0, 0.0),
@@ -62,7 +63,67 @@ fn spawn_obstacles(
                 base_color_texture: Some(assets.load("grid_textures/Blue/g1800.png").clone()),
                 ..default()
             }),
-            transform: Transform::from_xyz(-2.5, 0.5, -2.5),
+            transform: Transform::from_xyz(-1.5, 0.0, -2.5),
+            ..default()
+        },
+        Name::new("Blue cube"),
+        RigidBody::Fixed,
+        Collider::cuboid(0.5, 0.5, 0.5),
+    );
+
+    let blue_cube2 = (
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(assets.load("grid_textures/Blue/g1800.png").clone()),
+                ..default()
+            }),
+            transform: Transform::from_xyz(-0.5, -0.1, -2.5),
+            ..default()
+        },
+        Name::new("Blue cube"),
+        RigidBody::Fixed,
+        Collider::cuboid(0.5, 0.5, 0.5),
+    );
+
+    let blue_cube3 = (
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(assets.load("grid_textures/Blue/g1800.png").clone()),
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.5, -0.2, -2.5),
+            ..default()
+        },
+        Name::new("Blue cube"),
+        RigidBody::Fixed,
+        Collider::cuboid(0.5, 0.5, 0.5),
+    );
+
+    let blue_cube4 = (
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(assets.load("grid_textures/Blue/g1800.png").clone()),
+                ..default()
+            }),
+            transform: Transform::from_xyz(1.5, -0.3, -2.5),
+            ..default()
+        },
+        Name::new("Blue cube"),
+        RigidBody::Fixed,
+        Collider::cuboid(0.5, 0.5, 0.5),
+    );
+
+    let blue_cube5 = (
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(assets.load("grid_textures/Blue/g1800.png").clone()),
+                ..default()
+            }),
+            transform: Transform::from_xyz(2.5, -0.4, -2.5),
             ..default()
         },
         Name::new("Blue cube"),
@@ -86,5 +147,9 @@ fn spawn_obstacles(
     );
 
     commands.spawn(blue_cube);
+    commands.spawn(blue_cube2);
+    commands.spawn(blue_cube3);
+    commands.spawn(blue_cube4);
+    commands.spawn(blue_cube5);
     commands.spawn(yellow_cube);
 }
